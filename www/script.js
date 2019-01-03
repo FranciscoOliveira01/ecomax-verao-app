52a0b0683513a09974075f224488c30378eddd15
Leo Moreira <mailvolpe@gmail.com>
2019-01-03 17:10:35 -0300

    initial_commit

---
+++ www/script.js
@@ -0,0 +1,6 @@
+$(document).ready( function(){
+	$("#my_iframe").on("load" , function(){
+        $("#layer").hide('slow')
+        //$("#all-hoteis").css("display","block")//exibe botao 'ver todos os hoteis'
+  })	
+})
