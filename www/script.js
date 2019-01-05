d94631d057adcc8c2ea4d712948a0d59b4676f4a
franciscooliver <frandosax@gmail.com>
2019-01-05 11:46:36 -0300

    update_script2

--- www/script.js
+++ www/script.js
@@ -4,10 +4,17 @@ $(document).ready( function(){
 $("#my_iframe").on("load" , function(){
         $("#layer").hide('slow')
         mostrou = true;
-       
+
+        $("#my_iframe").contents().find("#nossas-redes-sociais .imagem").click(function(){
+        	var link = $("#my_iframe").contents().find("#nossas-redes-sociais .imagem").attr('href')
+        	navigator.app.loadUrl( link, { openExternal:true } );
+        })
+		   
   })
 
-if(mostrou){
-	 $("#layer").css("display", "none")
-}
+	if(mostrou){
+		 $("#layer").css("display", "none")
+	}
+
+	
 })
