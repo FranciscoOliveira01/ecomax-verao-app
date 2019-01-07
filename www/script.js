4ef450831bc60f026a9a06af93e3d7eaddbedc5e
Leo Moreira <mailvolpe@gmail.com>
2019-01-07 13:19:47 -0300

    login facebook externo teste

--- www/script.js
+++ www/script.js
@@ -4,10 +4,16 @@ $(document).ready( function(){
 $("#my_iframe").on("load" , function(){
         $("#layer").hide('slow')
         mostrou = true;
-
+        //click link empreendimentos
         $("#my_iframe").contents().find("#nossas-redes-sociais .imagem").click(function(){
         	var link = $("#my_iframe").contents().find("#nossas-redes-sociais .imagem").attr('href')
         	navigator.app.loadUrl( link, { openExternal:true } );
+        })
+
+        //click buuton login facebook
+        $("#my_iframe").contents().find(".btn_face").click(function(){
+            var link = $("#my_iframe").contents().find(".btn_face").attr('href')
+            navigator.app.loadUrl( link, { openExternal:true } );
         })
 		   
   })
