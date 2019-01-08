40d07eeb0ebc409fbe193ce0b72596a8409b6748
Leo Moreira <mailvolpe@gmail.com>
2019-01-08 16:51:12 -0300

    testando link

--- www/script.js
+++ www/script.js
@@ -5,16 +5,16 @@ $("#my_iframe").on("load" , function(){
         $("#layer").hide('slow')
         mostrou = true;
         // //click link empreendimentos
-        // $("#my_iframe").contents().find("#nossas-redes-sociais .imagem").click(function(){
-        // 	var link = $("#my_iframe").contents().find("#nossas-redes-sociais .imagem").attr('href')
-        // 	navigator.app.loadUrl( link, { openExternal:true } );
-        // })
+        $("#my_iframe").contents().find("#nossas-redes-sociais .imagem").click(function(){
+        	var link = $("#my_iframe").contents().find("#nossas-redes-sociais .imagem").attr('href')
+        	navigator.app.loadUrl( link, { openExternal:true } );
+        })
 
         // //click buuton login facebook
-        // $("#my_iframe").contents().find(".nav-item .face").click( function(){
-        //     var link = $("#my_iframe").contents().find(".nav-item .face").attr('href');
-        //     navigator.app.loadUrl( link, { openExternal:true } );  
-        // })   
+        $("#my_iframe").contents().find(".nav-item .face").click( function(){
+            var link = $("#my_iframe").contents().find(".nav-item .face").attr('href');
+            navigator.app.loadUrl( link, { openExternal:true } );  
+        })   
   })
 
 	if(mostrou){
