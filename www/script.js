2f6c20a6908b45ab41f12f642d07392ee6246345
Leo Moreira <mailvolpe@gmail.com>
2019-01-08 09:15:11 -0300

    disable

--- www/script.js
+++ www/script.js
@@ -5,18 +5,18 @@ $("#my_iframe").on("load" , function(){
         $("#layer").hide('slow')
         mostrou = true;
         //click link empreendimentos
-        $("#my_iframe").contents().find("#nossas-redes-sociais .imagem").click(function(){
-        	var link = $("#my_iframe").contents().find("#nossas-redes-sociais .imagem").attr('href')
-        	navigator.app.loadUrl( link, { openExternal:true } );
-        })
+        // $("#my_iframe").contents().find("#nossas-redes-sociais .imagem").click(function(){
+        // 	var link = $("#my_iframe").contents().find("#nossas-redes-sociais .imagem").attr('href')
+        // 	navigator.app.loadUrl( link, { openExternal:true } );
+        // });
 
-        //click buuton login facebook
-        $("#my_iframe").contents().find(".face").click( function(){
+        //click button login facebook
+        // $("#my_iframe").contents().find(".face").click( function(){
            
-            var link = $("#my_iframe").contents().find(".face").attr('href');
-            navigator.app.loadUrl( link, { openExternal:true } );
-            return false;
-        })
+        //     var link = $("#my_iframe").contents().find(".face").attr('href');
+        //     navigator.app.loadUrl( link, { openExternal:true } );
+        //     return false;
+        // });
 		   
   })
 
